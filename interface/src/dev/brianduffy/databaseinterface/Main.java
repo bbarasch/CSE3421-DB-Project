25378be560c580c8821fcc183aa940689a3f1a61
Brian Duffy <bgd1112@gmail.com>
2021-10-05 12:49:19 -0400

    More interface work

--- interface/src/dev/brianduffy/databaseinterface/Main.java
+++ interface/src/dev/brianduffy/databaseinterface/Main.java
@@ -20,20 +20,32 @@ class DatabaseDisplay extends JFrame {
     public DatabaseDisplay() {
         super("Database Interface");
 
-        child = new MainMenu();
+        child = new MainMenu(this);
         add(child);
 
-        this.pack();
-        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        this.setVisible(true);
+        pack();
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setVisible(true);
+    }
+
+    public void changeView(JPanel view) {
+        getContentPane().removeAll();
+        child = view;
+        add(child);
+
+        pack();
+        repaint();
     }
 }
 
 class MainMenu extends JPanel implements ActionListener {
-    private JButton search, addRecords, orderItems, editRecords, reports;
+    private final JButton search, addRecords, orderItems, editRecords, reports;
+    private final DatabaseDisplay display;
 
-    public MainMenu() {
+    public MainMenu(DatabaseDisplay display) {
         super(new GridLayout(5, 1));
+
+        this.display = display;
 
         search = new JButton("a.\tSearch");
         addRecords = new JButton("b.\tAdd new records");
@@ -42,6 +54,10 @@ class MainMenu extends JPanel implements ActionListener {
         reports = new JButton("e.\tUseful Reports");
 
         search.addActionListener(this);
+        addRecords.addActionListener(this);
+        orderItems.addActionListener(this);
+        editRecords.addActionListener(this);
+        reports.addActionListener(this);
 
         add(search);
         add(addRecords);
@@ -54,6 +70,23 @@ class MainMenu extends JPanel implements ActionListener {
     public void actionPerformed(ActionEvent e) {
         if (e.getSource().equals(search)) {
             System.out.println("Got Search Button");
+            display.changeView(new Search());
+        } else if (e.getSource().equals(addRecords)) {
+            System.out.println("Got Add Button");
+        } else if (e.getSource().equals(orderItems)) {
+            System.out.println("Got Order Button");
+        } else if (e.getSource().equals(editRecords)) {
+            System.out.println("Got Edit Button");
+        } else if (e.getSource().equals(reports)) {
+            System.out.println("Got Reports Button");
         }
+    }
+}
+
+class Search extends JPanel {
+    public Search() {
+        super(new GridLayout(1, 1));
+        
+        add(new JButton("Hello World"));
     }
 }
