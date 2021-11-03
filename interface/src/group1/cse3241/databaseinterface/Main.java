471d451493a1756e6bbf5ba569f4dbe9820fee3d
Ben Barasch <baraschben@gmail.com>
2021-11-03 19:18:46 -0400

    Connection to DB
    Added logic to connect to DB, copied from the lab lol

--- interface/src/group1/cse3241/databaseinterface/Main.java
+++ interface/src/group1/cse3241/databaseinterface/Main.java
@@ -1,7 +1,87 @@
 package group1.cse3241.databaseinterface;
 
+import java.sql.Connection;
+import java.sql.DatabaseMetaData;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.ResultSetMetaData;
+import java.sql.SQLException;
+import java.sql.Statement;
+
 public class Main {
+	// database name
+	private static String DATABASE = "Library.db";
+	
+	 /**
+     * Connects to the database if it exists, creates it if it does not, and returns the connection object.
+     * 
+     * @param databaseFileName the database file name
+     * @return a connection object to the designated database
+     */
+    public static Connection initializeDB(String databaseFileName) {
+    	/**
+    	 * The "Connection String" or "Connection URL".
+    	 * 
+    	 * "jdbc:sqlite:" is the "subprotocol".
+    	 * (If this were a SQL Server database it would be "jdbc:sqlserver:".)
+    	 */
+        String url = "jdbc:sqlite:" + databaseFileName;
+        Connection conn = null; // If you create this variable inside the Try block it will be out of scope
+        try {
+            conn = DriverManager.getConnection(url);
+            if (conn != null) {
+            	// Provides some positive assurance the connection and/or creation was successful.
+                DatabaseMetaData meta = conn.getMetaData();
+                System.out.println("The driver name is " + meta.getDriverName());
+                System.out.println("The connection to the database was successful.");
+            } else {
+            	// Provides some feedback in case the connection failed but did not throw an exception.
+            	System.out.println("Null Connection");
+            }
+        } catch (SQLException e) {
+            System.out.println(e.getMessage());
+            System.out.println("There was a problem connecting to the database.");
+        }
+        return conn;
+    }
+    
+    /**
+     * Queries the database and prints the results.
+     * 
+     * @param conn a connection object
+     * @param sql a SQL statement that returns rows
+     * This query is written with the Statement class, typically 
+     * used for static SQL SELECT statements
+     */
+    public static void sqlQuery(Connection conn, String sql){
+        try {
+        	Statement stmt = conn.createStatement();
+        	ResultSet rs = stmt.executeQuery(sql);
+        	ResultSetMetaData rsmd = rs.getMetaData();
+        	int columnCount = rsmd.getColumnCount();
+        	for (int i = 1; i <= columnCount; i++) {
+        		String value = rsmd.getColumnName(i);
+        		System.out.print(value);
+        		if (i < columnCount) System.out.print(",  ");
+        	}
+			System.out.print("\n");
+        	while (rs.next()) {
+        		for (int i = 1; i <= columnCount; i++) {
+        			String columnValue = rs.getString(i);
+            		System.out.print(columnValue);
+            		if (i < columnCount) System.out.print(",  ");
+        		}
+    			System.out.print("\n");
+        	}
+        } catch (SQLException e) {
+            System.out.println(e.getMessage());
+        }
+    }
+    
     public static void main(String[] args) {
+    	System.out.println("Establishing connection...");
+    	Connection conn = initializeDB(DATABASE);
+    	
         new DatabaseDisplay();
     }
 }
